78601b56e942e15ddae68bfb5c8a200ba8d8dcbc
Javier Murua <javierantoniomurua@gmail.com>
2026-05-17 02:19:12 -0300

    feat(seo): consolidate head, add OG absolute URLs, schema.org JSON-LD, fix canonical & twitter tags, update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://javiermurua.github.io",
+  site: "https://psipriscilagimenez.com",
 
   integrations: [
     tailwind(),
